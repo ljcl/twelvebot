758609ee397d2c19080ff812263cf449c291ef8b
Luke Clark <luke@lukeclark.com.au>
2022-06-30 21:04:01 +1000

    simplify, add cron job

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,67 @@
-import { Client } from "@typeit/discord";
+// Require the necessary discord.js classes
+import { Client, Intents } from 'discord.js';
+import { responseRandomiser } from './utils/responseRandomiser';
+import { CronJob } from 'cron';
 
-export class Main {
-  private static _client: Client;
-
-  static get Client(): Client {
-    return this._client;
-  }
-
-  static start(): void {
-    if (!process.env.DISCORD_BOT_ID) {
-      throw new Error(
-        "You need to set the `DISCORD_BOT_ID` environment variable"
-      );
-    }
-    this._client = new Client();
-
-    this._client.login(
-      process.env.DISCORD_BOT_ID,
-      `${__dirname}/tasks/*.ts`,
-      `${__dirname}/tasks/*.js`
-    );
-  }
+if (!process.env.DISCORD_BOT_ID) {
+  throw new Error('You need to set the `DISCORD_BOT_ID` environment variable');
 }
 
-Main.start();
+var job = new CronJob(
+  '00 16 * * 5',
+  async function () {
+    const channel = await client.channels.fetch('157040349972267008');
+    if (channel) {
+      // @ts-ignore
+      await channel.send(
+        'https://i.kym-cdn.com/entries/icons/original/000/021/686/Cn8qj2oVIAIIQhT.jpg'
+      );
+    }
+  },
+  null,
+  true,
+  'Australia/Sydney'
+);
+
+// Create a new client instance
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', async () => {
+  console.log('Ready!');
+
+  job.start();
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  responseRandomiser(message, /\bonion\b/i, [
+    'https://www.youtube.com/watch?v=Lt1u6N7lueM',
+  ]);
+  responseRandomiser(message, /\bbad at maths?\b/i, [
+    'https://i.imgur.com/ROS8oXA.jpg',
+  ]);
+  responseRandomiser(message, /\b(bevs?|beers?|bevington|beerio)\b/i, [
+    'https://i.imgur.com/8sGs5oL.jpg',
+  ]);
+  responseRandomiser(message, /\b(rube|rube\s?mobile)\b/i, [
+    'https://i.imgur.com/tNQB1wq.png',
+  ]);
+  responseRandomiser(message, /\bfriday lizard\b/i, [
+    'https://i.imgur.com/fcYoyDr.jpg',
+  ]);
+  responseRandomiser(message, /\bthere it is\b/i, [
+    'https://i.imgur.com/RG0BS1U.gifv',
+  ]);
+  responseRandomiser(message, /\bfinds a way\b/i, [
+    'https://i.imgur.com/ZXn0rOu.gifv',
+    'https://i.imgur.com/lNpjSrz.gifv',
+  ]);
+});
+
+client.login(process.env.DISCORD_BOT_ID);
--- src/utils/responseRandomiser.ts
+++ src/utils/responseRandomiser.ts
@@ -1,6 +1,12 @@
-import { Message } from "discord.js";
+import { Message } from 'discord.js';
 
-/** Randomly pick one item from an array and reply to the discord message */
+const recentLog = new Map<string, boolean>();
+
+/**
+ * Randomly pick one item from an array and reply to the discord message
+ *
+ * Won't send more than one response per channel/author every 10 seconds
+ */
 const responseRandomiser = (
   message: Message,
   /** Single word to trigger responses for */
@@ -9,7 +15,17 @@ const responseRandomiser = (
   responses: string[]
 ) => {
   if (matcher.exec(message.content)) {
-    message.reply(responses[Math.floor(Math.random() * responses.length)]);
+    const identifier = `${message.channelId}-${message.guildId}-${message.author.id}`;
+
+    if (!recentLog.get(identifier)) {
+      message.reply(responses[Math.floor(Math.random() * responses.length)]);
+    }
+
+    recentLog.set(identifier, true);
+
+    setTimeout(() => {
+      recentLog.delete(identifier);
+    }, 10000);
   }
 };
 
